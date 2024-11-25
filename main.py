1e35ff32e9f4c232536d296c2dab041391b111e3
“stars-spark” <cleverlijiale@163.com>
2024-11-25 21:47:56 +0800

    8

---
+++ main.py
@@ -0,0 +1,13 @@
+for value in range(1,11): #[1,10]
+    #range(upper,lower.step)
+    print(value)
+
+squares = []
+for value in range(1,11):
+    square = value ** 2
+    squares.append(square) #append方法
+    print(square)
+
+
+
+
